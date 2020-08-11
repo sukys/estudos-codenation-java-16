9286e161dbd545fea726c93bc252ea383c896f8e
Luis Sukys <luis.sukys@sippulse.com>
2020-08-10 22:19:16 -0300

    desafio completo.

---
+++ src/main/java/br/com/codenation/Order.java
@@ -0,0 +1,27 @@
+package br.com.codenation;
+
+import br.com.codenation.paymentmethods.PaymentMethod;
+
+public class Order {
+
+    private Double price;
+    private PaymentMethod paymentMethod;
+
+    public Order(Double price, PaymentMethod paymentMethod) {
+        this.price = price;
+        this.paymentMethod = paymentMethod;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+    
+    public Double getCalculatedPrice() {
+    	return paymentMethod.getPaymentStrategy().calculate(this.price);
+    }
+    
+}
